6352293ffbf0c7ac19bd195d12345181db97cc27
Cínthia Jesus <cinthia.jesus@i-value.com.br>
2021-12-15 19:21:42 -0200

    first commit

---
+++ cypress/integration/cadastro/cadastro_sucesso.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+const faker = require('faker');
+const emailAdress = faker.internet.email()
+const cpf = require('cpf');
+
+describe('Cadastro com sucesso', () => {
+  beforeEach(() => {
+    cy.visit('https://sandbox.melhorenvio.com.br/')
+  })
+
+  it('Cadastro', () => {
+    const email = emailAdress
+    cy.get('.row-desktop > :nth-child(5) > .btn-border').should('be.visible').click()
+    cy.get('#iptNome').should('be.visible').type(faker.name.firstName())
+    cy.get('#iptEmail').should('be.visible').type(emailAdress)
+    cy.get('.button').should('be.visible').click()
+    cy.get('#iptSobrenome').should('be.visible').type(faker.name.lastName())
+    cy.get('#iptCPF').should('be.visible').type(cpf.generate())
+    cy.get('#iptDtNascimento').should('be.visible').type(Cypress.env('me').dados_cadastro.data)
+    cy.get('#iptCelular').should('be.visible').type(`11998${faker.phone.phoneNumber()}`)
+    cy.get('#iptConfirmaEmail').should('be.visible').type(emailAdress)
+    cy.get('#iptSenha').should('be.visible').type(Cypress.env('me').dados_cadastro.senha)
+    cy.get('#iptConfirmaSenha').should('be.visible').type(Cypress.env('me').dados_cadastro.senha)
+    cy.get('.checkLine > .inputBox > label').should('be.visible').click()
+    cy.get('.button').click()
+    cy.get('.button').click()
+    cy.get('#iptCepEmpresa').should('be.visible').type(Cypress.env('me').dados_cadastro.cep)
+    cy.get('#iptNumeroEmpresa').should('be.visible').type(faker.random.number())
+    cy.get('.button').click()
+    cy.get('.complete-registration__text--primary').contains('Parabéns!')
+  })
+})
